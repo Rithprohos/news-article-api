273a9c1f29416e5e8f46af67fd7cae50d7eb9f9f
Prohos <prohossomrith@gmail.com>
2017-06-10 23:25:33 +0700

    change connection

--- repository/connection/pg-connection.js
+++ repository/connection/pg-connection.js
@@ -1,12 +1,12 @@
 /**
  * Created by Prohos on 6/10/2017.
  */
-// let user = 'uagxoqkrhoefvy';
-// let password = '2b73eb05ec84228acc33f190110358f8f913528cf335f66988ff7e02c9065408';
-// let hostName = 'ec2-23-21-220-48.compute-1.amazonaws.com';
-// let dbName = 'dskhg8rg9ua2l';
+let user = 'jdngxmmawuexbc';
+let password = '22bd20a6c6374240fe3a757fe29095f321cb089133e9186114375f3753148d73';
+let hostName = 'ec2-54-197-232-155.compute-1.amazonaws.com';
+let dbName = 'd1brg0ggc39ru4';
 
 
 export let pgp = require('pg-promise')(/*options*/);
-export let db = pgp('postgres://postgres:prohos@localhost:5432/news');
-// let db = pgp(`postgres://${user}:${password}@${hostName}:5432/${dbName}`);
+// export let db = pgp('postgres://postgres:prohos@localhost:5432/news');
+let db = pgp(`postgres://${user}:${password}@${hostName}:5432/${dbName}`);
