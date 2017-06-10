e2b88c51e24f1fa87a9525fd4e7723e0e215081d
Prohos <prohossomrith@gmail.com>
2017-06-10 15:42:56 +0700

    change module name

--- query/articlequery.js
+++ query/articlequery.js
@@ -1,7 +1,7 @@
 /**
  * Created by Prohos on 6/9/2017.
  */
-import * as qData from './sqlquery/sqlquery'
+import * as queryData from './sqlquery/sqlquery'
 
 // let user = 'uagxoqkrhoefvy';
 // let password = '2b73eb05ec84228acc33f190110358f8f913528cf335f66988ff7e02c9065408';
@@ -13,8 +13,9 @@ let pgp = require('pg-promise')(/*options*/);
 let db = pgp('postgres://postgres:prohos@localhost:5432/news');
 // let db = pgp(`postgres://${user}:${password}@${hostName}:5432/${dbName}`);
 
+//get all article
 export let getAllArticle = (req, res, next) => {
-    db.any(qData.qAllArticle)
+    db.any(queryData.qAllArticle)
         .then(function (data) {
             res.status(200)
                 .json({
@@ -28,9 +29,10 @@ export let getAllArticle = (req, res, next) => {
         });
 };
 
+//get all article
 export let getSingleArticle = (req, res, next) => {
     let articleID = parseInt(req.params.id);
-    db.one('select * from tbl_article where articleid = $1', articleID)
+    db.one(queryData.qArticleById, articleID)
         .then(function (data) {
             res.status(200)
                 .json({
