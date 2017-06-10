10449b459fa7391e2184cbb7091b68e482a00f1e
Prohos <prohossomrith@gmail.com>
2017-06-10 15:38:57 +0700

    fix sql statement change to sql function

--- query/sqlquery/sqlquery.js
+++ query/sqlquery/sqlquery.js
@@ -2,21 +2,6 @@
  * Created by Prohos on 6/10/2017.
  */
 
-export let qAllArticle = `select articleid, articletitle , articledetail, articleimageurl,
-    (
-      SELECT array_to_json(array_agg(row_to_json(d)))
-      from (
-        select authorid, authorname
-        from tbl_author
-        where tbl_author.authorid= tbl_article.articleauthorid
-      ) d
-    ) as author,
-		(
-			SELECT array_to_json(array_agg(row_to_json(e)))
-			FROM (
-				SELECT categoryid , categoryname
-				FROM tbl_categories
-				WHERE tbl_categories.categoryid = tbl_article.articlecategory
-			) e
-		) as category
-  from tbl_article`;
+export let qAllArticle = 'SELECT * FROM getAllArticle()';
+
+export let qArticleById = 'SELECT * FROM getArticleById($1)';
