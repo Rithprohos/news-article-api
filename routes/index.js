e428d962638d4f01804958ea452bf8ef98091ad4
Prohos <prohossomrith@gmail.com>
2017-06-10 23:42:28 +0700

    add

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Article News API' });
+  // res.render('index', { title: 'Article News API' });
 });
 
 module.exports = router;
