cbc5b959f3b10baf79f9ee4fd8f27411da881ee5
Prohos <prohossomrith@gmail.com>
2017-06-10 19:50:48 +0700

    add new statement

--- repository/query/sqlquery/sql-statement.js
+++ repository/query/sqlquery/sql-statement.js
@@ -6,8 +6,11 @@ export let qAllArticle = 'SELECT * FROM getAllArticle()';
 
 export let qArticleById = 'SELECT * FROM getArticleById($1)';
 
+export let qCreateArticle = 'SELECT createArticle(${title},${detail},${imgurl},${authid},${cateid})';
+
 export let qArticleByCategory = 'SELECT * FROM getArticleByCategory($1)';
 
 export let qAllCategory = 'SELECT * FROM tbl_categories';
 
 export let qCreateCategory = 'INSERT INTO tbl_categories(categoryname) VALUES(${name})';
+
