4d33eb0992d8bb35e16f44bbe3de2b4c804c64fb
Prohos <prohossomrith@gmail.com>
2017-06-12 00:08:56 +0700

    Merge branch 'master' of into server
    # Conflicts:
    #	repository/connection/pg-connection.js

--- repository/connection/pg-connection.js
+++ repository/connection/pg-connection.js
@@ -1,12 +1,12 @@
 /**
  * Created by Prohos on 6/10/2017.
  */
-let user = 'jdngxmmawuexbc';
-let password = '22bd20a6c6374240fe3a757fe29095f321cb089133e9186114375f3753148d73';
-let hostName = 'ec2-54-197-232-155.compute-1.amazonaws.com';
-let dbName = 'd1brg0ggc39ru4';
+// let user = 'jdngxmmawuexbc';
+// let password = '22bd20a6c6374240fe3a757fe29095f321cb089133e9186114375f3753148d73';
+// let hostName = 'ec2-54-197-232-155.compute-1.amazonaws.com';
+// let dbName = 'd1brg0ggc39ru4';
 
 
 export let pgp = require('pg-promise')(/*options*/);
-// export let db = pgp('postgres://postgres:prohos@localhost:5432/news');
-export let db = pgp(`postgres://${user}:${password}@${hostName}:5432/${dbName}`);
+export let db = pgp('postgres://postgres:prohos@localhost:5432/news');
+// export let db = pgp(`postgres://${user}:${password}@${hostName}:5432/${dbName}`);
