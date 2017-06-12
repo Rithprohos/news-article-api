5ff5a025ec433aca0cd7485a9fa0600486d6d6cd
Som Rithprohos <prohossomrith@icloud.com>
2017-06-12 10:37:06 +0700

    add

--- repository/connection/pg-connection.js
+++ repository/connection/pg-connection.js
@@ -5,7 +5,7 @@ let user = 'jdngxmmawuexbc';
 let password = '22bd20a6c6374240fe3a757fe29095f321cb089133e9186114375f3753148d73';
 let hostName = 'ec2-54-197-232-155.compute-1.amazonaws.com';
 let dbName = 'd1brg0ggc39ru4';
-
+let g='ghh';
 
 export let pgp = require('pg-promise')(/*options*/);
 // export let db = pgp('postgres://postgres:prohos@localhost:5432/news');
